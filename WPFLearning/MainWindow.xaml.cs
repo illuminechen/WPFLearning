17a2d97b0cab6c245a0e3285ba706d1ccf1d1823
illuminechen <illuminechen@gmail.com>
2021-01-23 13:19:29 +0800

    Tutorials: 01

--- WPFLearning/MainWindow.xaml.cs
+++ WPFLearning/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace WPFLearning
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            comboFinish.SelectedIndex = 0;
+        }
+
+        private void BtnApply_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The Description is: {txtDesctiption.Text}");
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            this.chkAssembly.IsChecked = false;
+            this.chkDrill.IsChecked = false;
+            this.chkFold.IsChecked = false;
+            this.chkLaser.IsChecked = false;
+            this.chkLathe.IsChecked = false;
+            this.chkPlasma.IsChecked = false;
+            this.chkPurchase.IsChecked = false;
+            this.chkRoll.IsChecked = false;
+            this.chkSaw.IsChecked = false;
+            this.chkWeld.IsChecked = false;
+        }
+
+        private void chkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            txtLength.Text += ((CheckBox)sender).Content;
+        }
+
+        private void Finish_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (txtNote == null)
+                return;
+
+            var combo = sender as ComboBox;
+            var value = combo.SelectedValue as ComboBoxItem;
+
+            txtNote.Text = value.Content.ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtMass.Text = txtSupplier.Text;
+        }
     }
 }
