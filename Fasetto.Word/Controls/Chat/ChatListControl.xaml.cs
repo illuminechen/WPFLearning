c7f70a2fe0a7ecf3991a1bb1b325a2904d4118d5
illuminechen <illuminechen@gmail.com>
2021-01-28 12:47:04 +0800

    Tutorials: 10 - ItemsControl Chat List & Design Time Data

---
+++ Fasetto.Word/Controls/Chat/ChatListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// ChatListControl.xaml 的互動邏輯
+    /// </summary>
+    public partial class ChatListControl : UserControl
+    {
+        public ChatListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Fasetto.Word/Controls/Chat/ChatListUserControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// ChatListUserControl.xaml 的互動邏輯
+    /// </summary>
+    public partial class ChatListUserControl : UserControl
+    {
+        public ChatListUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
